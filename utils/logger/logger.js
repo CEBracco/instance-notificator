2edeb3ce283b96ae349203f97e4219d3cb86f5eb
cbracco <cbracco@cespi.unlp.edu.ar>
2019-06-19 14:07:32 -0300

    File logging

--- utils/logger/logger.js
+++ utils/logger/logger.js
@@ -1,9 +1,15 @@
 var log4js = require('log4js');
 var loggerLevel = 'debug';
+log4js.configure({
+  appenders: { 
+    file: { type: 'file', filename: 'ws.log' }, 
+    out: { type: 'stdout' } 
+  },
+  categories: { default: { appenders: ['file','out'], level: loggerLevel } }
+});
+var logger = log4js.getLogger();
 
 function getLogger(){
-  var logger = log4js.getLogger();
-  logger.level = loggerLevel;
   return logger;
 }
 
